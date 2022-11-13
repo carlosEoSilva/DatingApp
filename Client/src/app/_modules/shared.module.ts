d8bf354a663da47b3fef7742fc21604f7a73f651
carlos.silva0000 <carlos.silva0000@totvs.com.br>
2022-11-13 16:29:09 -0300

    fim da sessao 6

---
+++ Client/src/app/_modules/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [ 
+    CommonModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({positionClass:'toast-bottom-right'})
+  ],
+  exports:[
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
